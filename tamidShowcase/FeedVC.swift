7d723a1e6cee4f3dfe70fc4204f638faa9daa572
helenzhangg <hz96@bu.edu>
2016-06-27 01:49:39 -0400

    added files for the UI post bar

--- tamidShowcase/FeedVC.swift
+++ tamidShowcase/FeedVC.swift
@@ -9,10 +9,16 @@
 import UIKit
 import Firebase
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var postField: MaterialTextField!
+    @IBOutlet weak var imageSelectorImage: UIImageView!
+    
     var posts = [Post]()
+    var imagePicker: UIImagePickerController!
+    
+    static var imageCache = NSCache() // creates only one instance of the post as static var
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,11 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.delegate = self
         tableView.dataSource = self
         
+        tableView.estimatedRowHeight = 355
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self 
+            
         // anytime data is changed or added app will sync automatically
         
         DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: {snapshot in
@@ -42,8 +53,6 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             
             self.tableView.reloadData() //reload anytime there is new data
         })
-        
-        
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -57,10 +66,52 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let post = posts[indexPath.row] // for each post let there be another cell
-        print(post.postDescription)
         
-        
-        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+        if let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as? PostCell {
+            
+            cell.request?.cancel() // if user scrolls past cell, cancel image request for the
+                                   // previous image and request img for the new cell
+            
+            var img: UIImage?
+            
+            if let url = post.imageUrl { // if img already exists in cache, get its key (url)
+                
+                img = FeedVC.imageCache.objectForKey(url) as? UIImage
+                // not "self.imageCache" since it is a public static variable
+            }
+            
+            cell.configureCell(post, img: img)
+            return cell // grab post and return it in cell
+            
+        } else {
+            return PostCell()
+        }
     }
     
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        let post = posts[indexPath.row]
+        
+        if post.imageUrl == nil { // if there is no image with the post, make cell shorter
+            
+            return 170
+            
+        } else {
+            return tableView.estimatedRowHeight
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        imageSelectorImage.image = image
+    }
+    
+    @IBAction func selectImage(sender: UITapGestureRecognizer) {
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func makePost(sender: AnyObject) {
+    }
 }
+
